f461580d88a82ca909ac26f44e0a96f9ebb4173a
BrianHSL-256 <briansorianoleal@gmail.com>
2026-07-24 23:47:53 -0600

    Fixed 2

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "./app";
-import {connectDB } from "./config/db"; 
+import app from "../src/app";
+import {connectDB } from "../src/config/db"; 
 
 
 let cached = globalThis as any;
